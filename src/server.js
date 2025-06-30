e54574b35e48e2e69f8d8818f8590a92026b5b8f
bigyan224 <bigyanacharya224@gmail.com>
2025-06-30 19:43:04 +0530

    cron job

--- src/server.js
+++ src/server.js
@@ -4,8 +4,12 @@ import ratelimiter from "./middleware/rateLimiter.js";
 import { initDB } from "./config/db.js";
 
 import transactionsRoute from "./routes/transactionsroute.js"
+import job from "./config/cron.js";
+
 dotenv.config();
 const app = express();
+
+if(process.env.NODE_ENV === "production") job.start();
 
 app.use(ratelimiter)
 app.use(express.json());
@@ -13,7 +17,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 5001;
 
 app.get("/test",(req,res)=>{
-    res.send("its working");
+    res.status(200.).json({status:"ok"});
 })
 
 
